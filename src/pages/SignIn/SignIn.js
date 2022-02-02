566fae66f8e98e6da42e108e94eb8437d63fc184
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-01 22:20:26 -0300

    feat/test: basic form handling in '/sign-in'

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { Container, StencilLogo, FormContainer, UserForm, Input, WideButton } from '../../components/SignUser/SignUser_styles.js';
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({ email:'', password:'' });
+  
+  /*
+  const [stillLoading, SetStillLoading] = useState(false);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    
+  })
+  */
+
+  function handleFormChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('clicked "Entrar"');
+    alert(`send formData (${formData.email}, ${formData.password})`);
+    //navigate();
   }
 
   return (
     <Container>
+      {console.log(formData)}
       <FormContainer>
         <StencilLogo>MyWallet</StencilLogo>
         <UserForm onSubmit={handleSubmit}>
           <Input required
             type='email'
-            placeholder='E-mail' />
+            placeholder='E-mail'
+            name='email'
+            value={formData.email}
+            onChange={handleFormChange}
+          />
           <Input required
             type='password'
-            placeholder='Senha' />
+            placeholder='Senha'
+            name='password'
+            value={formData.password}
+            onChange={handleFormChange}
+          />
           <WideButton type='submit'>
             Entrar
           </WideButton>
