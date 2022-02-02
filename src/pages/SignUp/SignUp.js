e0cfca91d70713d6c6b288fdb9bb3ee496d710b5
Tarcísio Teles <tfst@ecomp.poli.br>
2022-02-01 23:12:44 -0300

    feat/test: basic form handling in '/sign-up'

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,32 +1,59 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 
 import { Container, StencilLogo, FormContainer, UserForm, Input, WideButton } from '../../components/SignUser/SignUser_styles.js';
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({ name: '', email: '', password: '', passwordConfirmation: ''});
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log('clicked "Cadastrar"');
+    if (formData.passwordConfirmation !== formData.password) {
+      alert('senhas não conferem');
+    }
+    else {
+      alert(`send formData (${formData.name}, ${formData.email}, ${formData.password})`);
+    }
+  }
+
+  function handleFormChange(event) {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
   }
 
   return (
     <Container>
+      {console.log(formData)}
       <FormContainer>
         <StencilLogo>MyWallet</StencilLogo>
         <UserForm onSubmit={handleSubmit}>
           <Input required
             type='text'
-            placeholder='Nome' />
+            placeholder='Nome'
+            name='name'
+            value={formData.name}
+            onChange={handleFormChange}
+          />
           <Input required
             type='email'
-            placeholder='E-mail' />
+            placeholder='E-mail'
+            name='email'
+            value={formData.email}
+            onChange={handleFormChange}
+          />
           <Input required
             type='password'
-            placeholder='Senha' />
+            placeholder='Senha'
+            name='password'
+            value={formData.password}
+            onChange={handleFormChange}
+          />
           <Input required
             type='password'
-            placeholder='Confirme a senha' />
+            placeholder='Confirme a senha'
+            name='passwordConfirmation'
+            value={formData.passwordConfirmation}
+            onChange={handleFormChange}
+          />
           <WideButton type='submit'>
             Cadastrar
           </WideButton>
